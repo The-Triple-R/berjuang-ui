2bd778dd3ef0a538bc300469034a78abea794524
ifarbie <farizrifkyberliano@gmail.com>
2025-02-20 18:54:02 +0700

    chore(deps): add neobrutalism/shadcn-ui for styling components, configure eslint.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,41 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: ['class'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        main: 'var(--main)',
+        overlay: 'var(--overlay)',
+        bg: 'var(--bg)',
+        bw: 'var(--bw)',
+        blank: 'var(--blank)',
+        text: 'var(--text)',
+        mtext: 'var(--mtext)',
+        border: 'var(--border)',
+        ring: 'var(--ring)',
+        ringOffset: 'var(--ring-offset)',
+
+        secondaryBlack: '#212121',
+      },
+      borderRadius: {
+        base: '5px',
+      },
+      boxShadow: {
+        shadow: 'var(--shadow)',
+      },
+      translate: {
+        boxShadowX: '4px',
+        boxShadowY: '4px',
+        reverseBoxShadowX: '-4px',
+        reverseBoxShadowY: '-4px',
+      },
+      fontWeight: {
+        base: '500',
+        heading: '700',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
