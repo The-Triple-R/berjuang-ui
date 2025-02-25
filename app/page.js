daff0754e4ba0bcc015aec6ab30817f549a5868c
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2025-02-26 01:36:41 +0700

    fix: fix responsive for feature section

--- app/page.js
+++ app/page.js
@@ -6,75 +6,118 @@ import { AiFillSafetyCertificate } from 'react-icons/ai';
 import { TbReport } from 'react-icons/tb';
 import { FaSquareXTwitter, FaInstagram, FaFacebookF } from 'react-icons/fa6';
 import { Button } from '@/components/ui/button';
+import Image from "next/image";
 
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className='relative flex flex-col gap-44 items-center justify-center bg-bg dark:bg-darkBg min-h-screen'>
-        <section id='home' className='min-h-screen flex flex-col items-center justify-center px-4 md:px-8 gap-5'>
-          <h3>
-            <Badge className='font-bold text-xl'>BerjUANG</Badge>
-          </h3>
-          <p className='font-bold text-6xl text-center'>Kendalikan Keuangan Bisnis Anda dengan AI</p>
-          <p className='text-lg text-center'>Kelola keuangan UMKM Anda dengan mudah dan efisien. BerjUANG memberikan insight berharga untuk membantu bisnis Anda tumbuh.</p>
+      <main className='flex flex-col justify-center dark:bg-darkBg min-h-screen'>
+        <section id="home" className="flex flex-col md:align-center md:flex-row min-h-screen bg-bg w-full gap-8 py-8 px-4 md:px-8">
+          <div className="flex flex-col gap-3 md:justify-center text-center md:text-start w-full">
+            <h2 className="font-extrabold text-4xl text-center md:text-start lg:text-5xl">
+              Kelola Keuangan Bisnis Anda dengan AI
+            </h2>
+            <p className="text-mtext md:text-start">
+              <span className="uppercase font-extrabold">Berjuang</span> dapat membantu mengelola keuangan UMKM dengan bantuan AI supaya menjadi lebih mudah, cepat, dan akurat dengan insight cerdas untuk pertumbuhan bisnis.
+            </p>
+          </div>
+
+          <div className="relative w-full flex justify-center">
+            <Image 
+              src="/images/people.png"
+              alt="People Image"
+              width={600} 
+              height={400} 
+              className="w-[80%] h-auto object-cover rounded-lg bg-black bg-opacity-10 backdrop-blur-md"
+              priority
+            />
+            <div className="absolute top-[20%] right-0 flex flex-col p-4 rounded-md bg-white bg-opacity-40 backdrop-blur-lg border border-white/30 text-black shadow-md">
+              <p className="text-[0.8rem] text-gray-800">Saldo Anda</p>
+              <p className="font-bold">Rp 20.000</p>
+            </div>
+            <div className="absolute top-[60%] left-0 flex flex-col p-4 rounded-md bg-white bg-opacity-40 backdrop-blur-lg border border-white/30 text-black shadow-md">
+              <p className="text-[0.8rem] text-gray-800">Pengeluaran Anda</p> 
+              <p className="font-bold">Rp 100.000</p> 
+            </div>
+          </div>
+        </section>
+        <section id='features' className='border-t-4 flex flex-col gap-8 py-[2rem] px-4 md:px-8 border-border dark:border-darkBorder'>
+          <div>
+            <h3 className='text-center text-4xl font-bold'>Our <span className='text-main'>Features</span></h3>
+            <p className='text-center text-mtext text-lg mt-2'>
+              Apa saja fitur yang diberikan oleh kami?
+            </p>
+          </div>
+
+          <div className='grid md:grid-cols-2 gap-8'>
+            <FeatureCard 
+              title='Analisis Keuangan Otomatis' 
+              description='Dapatkan analisis keuangan secara otomatis dengan AI yang mengidentifikasi tren dan pola pengeluaran bisnis Anda.'
+            >
+              <MdAutoAwesome />
+            </FeatureCard>
+
+            <FeatureCard 
+              title='Rekomendasi Keuangan Cerdas' 
+              description='Terima saran keuangan yang dipersonalisasi berdasarkan kondisi bisnis Anda untuk pengambilan keputusan yang lebih baik.'
+            >
+              <MdRecommend />
+            </FeatureCard>
+
+            <FeatureCard 
+              title='Laporan Keuangan Komprehensif' 
+              description='Akses laporan keuangan yang lengkap dan mudah dipahami untuk membantu Anda mengelola bisnis dengan lebih efektif.'
+            >
+              <TbReport />
+            </FeatureCard>
+
+            <FeatureCard 
+              title='Keamanan Data Terjamin' 
+              description='Data keuangan Anda dilindungi dengan enkripsi tingkat tinggi dan sistem keamanan canggih untuk menjaga privasi bisnis Anda.'
+            >
+              <AiFillSafetyCertificate />
+            </FeatureCard>
+          </div>
+        </section>
+        <section id='benefits' className='border-t-4 border-border dark:border-darkBorder p-10'>
+          <div>
+            <h3 className='text-center text-4xl font-bold'>Manfaat <span className='text-main'>Produk</span></h3>
+            <p className='text-center text-mtext text-lg mt-2'>
+              Apa yang Bisa kami lakukan untuk bisnis anda?
+            </p>
+          </div>
+          <div className='grid grid-cols-3 gap-5 justify-items-center'>
+            <div className='text-center flex flex-col gap-4 w-[250px]'>
+              <div className='flex justify-center'>
+                <MdAttachMoney size={64} />
+              </div>
+              <h6 className='w-[200px] flex mx-auto font-semibold'>Meningkatkan Efisiensi Keuangan</h6>
+              <p>Kelola keuangan bisnis Anda dengan lebih cepat dan mudah.</p>
+            </div>
+            <div className='text-center flex flex-col gap-4 w-[250px]'>
+              <div className='flex justify-center'>
+                <MdAccessTime size={64} />
+              </div>
+              <h6 className='w-[200px] flex mx-auto font-semibold'>Menghemat Waktu dan Biaya</h6>
+              <p>Kurangi waktu yang dihabiskan untuk mengelola keuangan dan fokus pada pengembangan bisnis Anda.</p>
+            </div>
+            <div className='text-center flex flex-col gap-4 w-[250px]'>
+              <div className='flex justify-center'>
+                <MdQuestionMark size={64} />
+              </div>
+              <h6 className='w-[200px] flex mx-auto font-semibold'>Mengambil Keputusan Tepat</h6>
+              <p>Dapatkan insight berharga untuk membuat keputusan keuangan yang lebih baik.</p>
+            </div>
+          </div>
+          <div className='flex justify-center mt-10'>
+            <Button className="text-xl">Coba Sekarang!</Button>
+          </div>
         </section>
       </main>
-      <div>
-        <section id='features' className='border-t-4 border-border dark:border-darkBorder font-semibold p-10'>
-          <div className='w-[1300px] max-w-full mx-auto'>
-            <h3 className='text-center text-2xl mb-16'>Features</h3>
-            <div className='grid grid-cols-2 gap-5'>
-              <FeatureCard description='Analisis Keuangan Otomatis'>
-                <MdAutoAwesome />
-              </FeatureCard>
-              <FeatureCard description='Rekomendasi Keuangan Cerdas'>
-                <MdRecommend />
-              </FeatureCard>
-              <FeatureCard description='Laporan Keuangan Komprehensif'>
-                <TbReport />
-              </FeatureCard>
-              <FeatureCard description='Keamanan Data Terjamin'>
-                <AiFillSafetyCertificate />
-              </FeatureCard>
-            </div>
-          </div>
-        </section>
-        <section className='border-t-4 border-border dark:border-darkBorder p-10'>
-          <div className='w-[1100px] max-w-full mx-auto'>
-            <h3 className='text-center text-2xl mb-1 font-semibold'>Manfaat Produk</h3>
-            <p className='text-center mb-9'>Apa yang Bisa BerjUANG Lakukan untuk Bisnis Anda?</p>
-            <div className='grid grid-cols-3 gap-5 justify-items-center'>
-              <div className='text-center flex flex-col gap-4 w-[250px]'>
-                <div className='flex justify-center'>
-                  <MdAttachMoney size={64} />
-                </div>
-                <h6 className='w-[200px] flex mx-auto font-semibold'>Meningkatkan Efisiensi Keuangan</h6>
-                <p>Kelola keuangan bisnis Anda dengan lebih cepat dan mudah.</p>
-              </div>
-              <div className='text-center flex flex-col gap-4 w-[250px]'>
-                <div className='flex justify-center'>
-                  <MdAccessTime size={64} />
-                </div>
-                <h6 className='w-[200px] flex mx-auto font-semibold'>Menghemat Waktu dan Biaya</h6>
-                <p>Kurangi waktu yang dihabiskan untuk mengelola keuangan dan fokus pada pengembangan bisnis Anda.</p>
-              </div>
-              <div className='text-center flex flex-col gap-4 w-[250px]'>
-                <div className='flex justify-center'>
-                  <MdQuestionMark size={64} />
-                </div>
-                <h6 className='w-[200px] flex mx-auto font-semibold'>Mengambil Keputusan Tepat</h6>
-                <p>Dapatkan insight berharga untuk membuat keputusan keuangan yang lebih baik.</p>
-              </div>
-            </div>
-            <div className='flex justify-center mt-10'>
-              <Button className="text-xl">Coba Sekarang!</Button>
-            </div>
-          </div>
-        </section>
-        <footer className='font-semibold bg-[#262626] text-footerText pt-16 px-10'>
-          <div className='w-[600px] max-w-full mx-auto'>
+      <footer className='font-semibold bg-[#262626] text-footerText pt-16 px-10'>
+          <div className='max-w-full mx-auto'>
             <div className='grid grid-cols-3 gap-5 text-xl border-b border-[#ffffff13] pb-16'>
               <div className='col-span-2'>
                 <h3>
@@ -95,7 +138,6 @@ export default function Home() {
             <p className='text-center py-7 font-base'>Copyright &copy; 2025 BerjUANG. | All rights reserved.</p>
           </div>
         </footer>
-      </div>
     </>
   );
 }
