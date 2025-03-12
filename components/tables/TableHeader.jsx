5bb9e06e6cd35151df9f32901785af2715e5e06c
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2025-03-12 22:50:46 +0700

    restyle: make header to bold

--- components/tables/TableHeader.jsx
+++ components/tables/TableHeader.jsx
@@ -1,6 +1,6 @@
 export default function TableHeader({ children, colSpan = 1 }) {
   return (
-    <th className="py-2 text-sm px-4 text-center text-[#171725] font-normal" colSpan={colSpan}>
+    <th className="py-2 text-sm px-4 text-center text-[#171725] font-bold" colSpan={colSpan}>
       {children}
     </th>
   );
